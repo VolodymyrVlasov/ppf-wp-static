f85c3414e81bddf5addb7f0fc552d865d077f45f
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2023-04-06 23:54:37 +0300

    add php

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,4 @@
-// const gulp = require('gulp');
-// const htmlmin = require('gulp-htmlmin');
-// const rename = require('gulp-rename');
-// const fileInclude = require('gulp-file-include');
-// const clean = require('gulp-clean');
-// const concat = require('gulp-concat');
-// const groupCssMediaQueries = require('gulp-group-css-media-queries');
 
-// gulp.task('devout', () => {
-//     return gulp.src(['dev/components/dev-templates/*.html'], { allowEmpty: true })
-//         // .pipe(htmlmin({ collapseWhitespace: true }))
-//         // .pipe(rename({ suffix: '.min' }))
-//         .pipe(fileInclude({
-//             prefix: '@@',
-//             basepath: '@file'
-//         }))
-//         .pipe(htmlmin({
-//             // collapseWhitespace: true,
-//             removeComments: true
-//         }))
-//         .pipe(gulp.dest('./dev-watch/components/'));
-// });
-
-// gulp.task('devout-clean', () => {
-//     return gulp.src('./dev-watch')
-//         .pipe(clean({ force: true }))
-// });
-
-// gulp.task('group-css-media', () => {
-//     return gulp.src('dev/styles/*')
-//         .pipe(concat('style.css'))
-//         .pipe(groupCssMediaQueries()) // Объединяем повторяющиеся медиа-запросы
-//         .pipe(gulp.dest('dev-watch/styles/'));
-// });
-
-
-
-
-
-// gulp.task('devwatch', () => {
-//     return gulp.watch('dev/**/*', gulp.series('devout-clean', 'devout'));
-// });
 
 // ***CSS***
 // gulp-scss -> action({outputStyle: "expanded"})
@@ -132,10 +91,6 @@ const changeUrlInCSS = () => {
         .pipe(gulp.dest('dist/styles'));
 }
 
-exports.changeSrcInHTML = changeSrcInHTML;
-exports.changeUrlInCSS = changeUrlInCSS;
-exports.default = gulp.series(changeSrcInHTML, changeUrlInCSS);
-
 const cssTask = () => {
     return gulp.src('dev/styles/*.css')
         .pipe(concat('style.css'))
@@ -147,17 +102,23 @@ const jsAppTask = () => {
         .pipe(gulp.dest('dist/wp-content/themes/paperfox/js/'))
 };
 
+const phpAppTask = () => {
+    return gulp.src("dev/pages/wp-content/**/*.php", { allowEmpty: true })
+        .pipe(dest("dist/wp-content/"))
+}
+
 const browserSync = () => {
-    return gulp.watch('dev/**/*', gulp.series(cssTask, htmlTask, jsAppTask, staticContentTask));
+    return gulp.watch('dev/**/*', gulp.series(cssTask, phpAppTask, htmlTask, jsAppTask, staticContentTask));
 };
 
 // let build = gulp.series(clean, cssTask, htmlTask);
 let watch = gulp.parallel(browserSync);
 
+exports.default = gulp.series(changeSrcInHTML, changeUrlInCSS);
 
+exports.phpAppTask = phpAppTask;
 exports.changeSrcInHTML = changeSrcInHTML;
-exports.changeSrcInHTML = changeSrcInHTML;
-
+exports.changeUrlInCSS = changeUrlInCSS;
 exports.jsAppTask = jsAppTask;
 exports.cleanTask = cleanTask;
 exports.cssTask = cssTask;
