8d8c9f631d9cba6305a5594af03ac6684333a4bb
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2023-04-16 02:42:09 +0300

    add 404 and fix deploy

---
+++ const.js
@@ -0,0 +1,9 @@
+export const wwwVar = {
+    '{{domain}}': 'https://paperfox.in.ua',
+    '{{sub_domain}}': 'https://shop.paperfox.in.ua',
+    '{{email}}': 'print@paperfox.in.ua',
+    '{{phone}}': '0800 332 042',
+    '{{viber}}': 'viber://add?number=380730472212',
+    '{{telegram}}': 'https://t.me/Paper_Fox',
+}
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,62 @@
-let vinylFtp = require("vinyl-ftp"),
-    util = require("gulp-util"),
-    gulp = require("gulp"),
-    fileInclude = require("gulp-file-include"),
-    concat = require("gulp-concat"),
-    replace = require("gulp-replace"),
-    webp = require("gulp-webp"),
-    webpHtml = require("gulp-webp-html-nosvg"),
-    htmlminify = require("gulp-html-minify"),
-    dotenv = require('dotenv');
+import vinylFtp from "vinyl-ftp";
+import util from "gulp-util";
+import gulp from "gulp";
+import fileInclude from "gulp-file-include";
+import concat from "gulp-concat";
+import replace from "gulp-replace";
+import webp from "gulp-webp";
+import webpHtml from "gulp-webp-html-nosvg";
+import htmlminify from "gulp-html-minify";
+import dotenv from "dotenv";
+import { deleteSync } from "del";
+import { wwwVar } from "./const.js"
 
-// Загружаем переменные окружения из файла .env
 dotenv.config();
 
-const buildWPTheme = (done) => {
-    gulp.src("dev/wp-theme/**/*.php", { allowEmpty: true })
-        .pipe(fileInclude({ prefix: '@@', basepath: '@file' }))
+const ftp = vinylFtp.create({
+    host: process.env.FTP_HOST,
+    user: process.env.FTP_USER,
+    password: process.env.FTP_PASSWORD,
+    parallel: 5,
+    log: util.log
+});
+
+export const clear = (done) => {
+    deleteSync(["paperfox/"]);
+    deleteSync(["www/"]);
+    deleteSync(["devPreview/"]);
+
+    ftp.rmdir("/paperfox.in.ua/shop/wp-content/themes/paperfox", (err) => {
+        if (err) {
+            console.error('Error deleting directory:', err);
+        } else {
+            console.log('Directory deleted successfully.');
+        }
+    });
+
+    ftp.rmdir("/paperfox.in.ua/www", (err) => {
+        if (err) {
+            console.error('Error deleting directory:', err);
+        } else {
+            console.log('Directory deleted successfully.');
+        }
+    });
+    done();
+}
+
+export const buildwp = (done) => {
+    // deleteSync(["paperfox/"]);
+
+    let stream = gulp.src("dev/wp-theme/**/*.php", { aloowEmpty: true })
+        .pipe(fileInclude({ prefix: '@@', basepath: '@file' }));
+
+    for (const [placeholder, value] of Object.entries(wwwVar)) {
+        stream = stream.pipe(replace(placeholder, value));
+    }
+
+    stream
         .pipe(webpHtml())
-        .pipe(replace('src="./static', 'src=" <?php echo get_template_directory_uri(); ?>/static'))
+        .pipe(replace('src="./static', 'src="<?php echo get_template_directory_uri();?>/static'))
         .pipe(gulp.dest("paperfox/"));
 
     gulp.src("dev/wp-theme/screenshot.png")
@@ -33,25 +73,43 @@ const buildWPTheme = (done) => {
     return done();
 }
 
-const buildStaticPages = (done) => {
-    gulp.src(["dev/pages/**/*.html", "!dev/pages/template-page.html", "!dev/pages/test.html"], { aloowEmpty: true })
-        .pipe(fileInclude({ prefix: '@@', basepath: '@file' }))
-        .pipe(webpHtml())
+export const buildpages = (done) => {
+    // deleteSync(["www/"]);
+
+    const srcHtml = ["dev/pages/**/*.html", "!dev/pages/template-page.html", "!dev/pages/test.html"];
+    const srcCSS = ['dev/styles/*.css', '!dev/styles/_1_wp-theme.css', '!dev/styles/style.css'];
+    const srcStatic = "dev/static/**/*.*";
+
+    gulp.src("dev/pages/.htaccess", { aloowEmpty: true })
+        .pipe(gulp.dest("www/"))
+
+    let stream = gulp.src(srcHtml, { aloowEmpty: true })
+        .pipe(fileInclude({ prefix: '@@', basepath: '@file' }));
+
+    for (const [placeholder, value] of Object.entries(wwwVar)) {
+        stream = stream.pipe(replace(placeholder, value));
+    }
+
+    stream.
+        pipe(webpHtml())
         .pipe(htmlminify())
         .pipe(gulp.dest("www/"));
 
-    gulp.src(['dev/styles/*.css', '!dev/styles/_1_wp-theme.css', '!dev/styles/style.css'])
+    gulp.src(srcCSS, { aloowEmpty: true })
         .pipe(concat('style.css'))
         .pipe(gulp.dest("www/"));
 
-    gulp.src("dev/static/**/*")
+    gulp.src(srcStatic, { aloowEmpty: true })
+        .pipe(gulp.dest("www/static/"))
         .pipe(webp({ quality: 90 }))
         .pipe(gulp.dest("www/static/"));
 
     return done();
 }
 
-const devStaticPages = (done) => {
+export const dev = (done) => {
+    // deleteSync(["devPreview/**", "!devPreview/"]);
+
     gulp.src(["dev/pages/**/*.html", "!dev/pages/template-page.html", "!dev/pages/test.html"], { aloowEmpty: true })
         .pipe(fileInclude({ prefix: '@@', basepath: '@file' }))
         .pipe(webpHtml())
@@ -75,48 +133,27 @@ const devStaticPages = (done) => {
     return done();
 }
 
-const deployWP = (done) => {
-    const ftp = vinylFtp.create({
-        host: process.env.FTP_HOST,
-        user: process.env.FTP_USER,
-        password: process.env.FTP_PASSWORD,
-        parallel: 5,
-        log: util.log
-    });
-
+export const deploywp = (done) => {
     const LOCAL = ['paperfox/**/*.*'];
     const REMOTE = "/paperfox.in.ua/shop/wp-content/themes/paperfox";
 
     gulp.src(LOCAL, {})
-        .pipe(ftp.newer(REMOTE)) // upload only newer files
         .pipe(ftp.dest(REMOTE));
 
     return done();
 }
 
-const deployWWWW = (done) => {
-    const ftp = vinylFtp.create({
-        host: process.env.FTP_HOST,
-        user: process.env.FTP_USER,
-        password: process.env.FTP_PASSWORD,
-        parallel: 5,
-        log: util.log
-    });
-
+export const deploywww = (done) => {
     const LOCAL = ['www/**/*.*'];
-    const REMOTE = "/paperfox.in.ua/www";
+    const REMOTE = "/paperfox.in.ua/www/";
 
     gulp.src(LOCAL, {})
-        .pipe(ftp.newer(REMOTE)) // upload only newer files
+        .pipe(ftp.dest(REMOTE));
+
+    gulp.src("www/.htaccess", { aloowEmpty: true })
         .pipe(ftp.dest(REMOTE));
 
     return done();
 }
 
-exports.buildtheme = buildWPTheme;
-exports.buildpages = buildStaticPages;
-exports.devwatch = devStaticPages;
-exports.deploywp = deployWP;
-exports.deploywww = deployWWWW;
-
-exports.default = gulp.series(devStaticPages);
+export default () => gulp.watch(["dev/", "!dev/wp-theme"], gulp.series(dev));
