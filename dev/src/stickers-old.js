fa12f0455247829dedb0d632e9f9a8489a206015
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2023-10-10 01:24:43 +0300

    update old roung calc script

--- dev/src/stickers-old.js
+++ dev/src/stickers-old.js
@@ -12,23 +12,23 @@ const roundSticker = {
 }
 
 const CUT_PRICE = {
-    INDEX: [],
-    KISS_CUT_A4: [],
-    KISS_CUT_A3: [],
-    DIE_CUT: []
+    INDEX: [1, 5, 10, 20, 40, 50, 100, 200, 400, 500, 1000],
+    KISS_CUT_A4: [6, 6, 4, 4, 4, 3, 2.5, 2.5, 2.5, 2.5, 1],
+    KISS_CUT_A3: [6, 6, 4, 4, 4, 3, 2.5, 2.5, 2.5, 2.5, 1],
+    DIE_CUT: [24, 24, 16, 16, 16, 14, 12, 12, 12, 12, 8,]
 }
 
 const MATERIAL_PRICE = {
-    INDEX: [],
-    PAPER_STICKER: [],
-    PAPER_STICKER_LAMINATED: [],
-    PAPER_STICKER_SOFT_TOUCH: [],
-    PET_STICKER: [],
-    CLEAR_PET_STICKER: [],
-    PET_STICKER: [],
-    PVC_STICKER: [],
-    PVC_CLEAR_STICKER: [],
-    PVC_STICKER_LAMINATED: []
+    INDEX: [1, 5, 10, 20, 40, 50, 100, 200, 400, 500, 1000],
+    PAPER_STICKER: [40, 40, 30, 28, 25, 15, 14, 13, 13, 13, 13],
+    PAPER_STICKER_LAMINATED: [80, 64, 49, 45, 42, 29, 26, 25, 25, 25, 25],
+    PAPER_STICKER_SOFT_TOUCH: [120, 110, 90, 68, 65, 45, 39, 33, 33, 33, 33],
+    PET_STICKER: [110, 83, 83, 77, 77, 75, 72, 69, 69, 69, 69],
+    CLEAR_PET_STICKER: [115, 85, 85, 79, 79, 76, 74, 71, 71, 71, 71],
+    CLEAR_MATT_PET_STICKER: [105, 78, 78, 73, 73, 70, 68, 65, 65, 65, 65],
+    PVC_STICKER: [55, 50, 45, 40, 40, 35, 30, 30, 30, 30, 30],
+    PVC_CLEAR_STICKER: [55, 50, 45, 40, 40, 35, 30, 30, 30, 30, 30],
+    PVC_STICKER_LAMINATED: [125, 90, 85, 80, 80, 70, 60, 60, 60, 60, 60]
 }
 
 const PRINT_AREA = {
@@ -41,60 +41,104 @@ const sizeInput = document.getElementById("size");
 const amountInput = document.getElementById("amount");
 const materialSelect = document.getElementById("material");
 const cutSelect = document.getElementById("cut");
+const sizeLabel = document.getElementById("size_label");
+const amountLabel = document.getElementById("amount_label");
+const resultCnt = document.getElementById("result");
 
-sizeInput.addEventListener("input", (e) => {
-    roundSticker.targetAmount = e.target.value;
-    {
-        amountInput.value = amountInput.step = roundSticker.stickersAtSheet;
-    }
+sizeInput.addEventListener("input", e => {
+    roundSticker.diameter = Number(e.target.value);
+    render();
 })
 
-amountInput.addEventListener("input", () => {
-    roundSticker.targetAmount = e.target.value;
+sizeInput.addEventListener("change", e => {
+    roundSticker.diameter = Number(e.target.value);
+    setTimeout(() => {
+
+        amountInput.value = roundSticker.stickersAtSheet;
+        amountLabel.innerHTML = `Кількість: ${roundSticker.stickersAtSheet} шт`;
+
+    }, 0);
+    Calculator.calculate();
 })
 
-
-materialSelect.addEventListener("input", () => {
-
+amountInput.addEventListener("input", e => {
+    roundSticker.targetAmount = Number(e.target.value);
+    Calculator.calculate();
 })
 
-cutSelect.addEventListener("input", () => {
+materialSelect.addEventListener("change", e => {
+    roundSticker.material = e.target.value;
+    Calculator.calculate();
+})
 
+cutSelect.addEventListener("change", e => {
+    roundSticker.cutType = e.target.value;
+    Calculator.calculate();
 })
 
 class Calculator {
-    static stickersAtSheet() {
+    static calcStickersAtSheet() {
         // =if((rounddown($D$49/($D40+$F$49*2)) * rounddown($E$49/($D40+$F$49*2))) * $G$49)
         const printArea = PRINT_AREA[roundSticker.cutType];
 
         roundSticker.stickersAtSheet =
-            Math.ceil(printArea.x / (roundSticker.diameter + printArea.bleed * 2)) *
-            Math.ceil(printArea.y / (roundSticker.diameter + printArea.bleed * 2)) *
+            Math.floor(printArea.x / (roundSticker.diameter + printArea.bleed * 2)) *
+            Math.floor(printArea.y / (roundSticker.diameter + printArea.bleed * 2)) *
             printArea.multiplier;
+
+        console.log(printArea.x, (roundSticker.diameter + printArea.bleed * 2), Math.floor(printArea.x / (roundSticker.diameter + printArea.bleed * 2))
+        );
+
+        console.log(printArea.y, (roundSticker.diameter + printArea.bleed * 2), Math.floor(printArea.y / (roundSticker.diameter + printArea.bleed * 2)));
+
+        amountInput.step = roundSticker.stickersAtSheet;
+        amountInput.min = roundSticker.stickersAtSheet;
+        amountInput.max = roundSticker.stickersAtSheet * 200;
     }
 
-    static cutAtSheet() {
+    static calcCutAtSheet() {
         // =roundup(0,00314*D40*K40)
-
         roundSticker.cutAtSheet =
             Math.ceil(0.00314 * roundSticker.diameter * roundSticker.stickersAtSheet);
     }
 
-    static sheetsInOrder() {
+    static calcSheetsInOrder() {
         // =if(I40>0; if(H40>1;roundup(G40/K40) * H40;roundup(G40/K40));1)
+        roundSticker.sheetsAtPrintingRun =
+            Math.ceil(roundSticker.targetAmount / roundSticker.stickersAtSheet);
+
+        roundSticker.amount = roundSticker.sheetsAtPrintingRun * roundSticker.stickersAtSheet;
     }
 
-    static cutInOrder() {
+    static calcCutInOrder() {
         // =M40*N40
+        roundSticker.cutAtPrintingRun =
+            roundSticker.cutAtSheet * roundSticker.sheetsAtPrintingRun;
     }
 
-    static calcPrice() {
+    static calculate() {
+        Calculator.calcStickersAtSheet();
+        Calculator.calcCutAtSheet();
+        Calculator.calcSheetsInOrder();
+        Calculator.calcCutInOrder();
 
+        let i = CUT_PRICE.INDEX.reverse().findIndex(element => roundSticker.cutAtPrintingRun >= element)
+        let j = MATERIAL_PRICE.INDEX.reverse().findIndex(element => roundSticker.sheetsAtPrintingRun >= element)
+
+        roundSticker.cutPrice = CUT_PRICE[roundSticker.cutType][i] * roundSticker.cutAtPrintingRun;
+        roundSticker.printPrice = MATERIAL_PRICE[roundSticker.material][j] * roundSticker.sheetsAtPrintingRun;
+
+        console.table(roundSticker);
+        render();
     }
-
-    render();
 }
 
 function render() {
+    sizeLabel.innerHTML = `Діаметр: ${roundSticker.diameter} мм`;
+    amountLabel.innerHTML = `Кількість: ${roundSticker.amount} шт`;
+    resultCnt.innerHTML = `
+    <span>Друк: ${roundSticker.sheetsAtPrintingRun} арк. - ${roundSticker.printPrice} грн.</span>
+    <span>Порізка: ${roundSticker.cutAtPrintingRun} м.п. - ${roundSticker.cutPrice} грн.</span>
+    <span>Вартість: ${roundSticker.printPrice + roundSticker.cutPrice} грн.</span>`
 
 }
