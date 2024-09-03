6d11217b528ac6d587b7da837a0e3cd27ead0ee3
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2024-09-03 23:18:38 +0300

    single-product

--- dev/wp-theme/functions.php
+++ dev/wp-theme/functions.php
@@ -20,10 +20,8 @@ function mytheme_add_woocommerce_support()
 {
     add_theme_support('woocommerce');
 }
+
 add_action('after_setup_theme', 'mytheme_add_woocommerce_support');
-
-
-
 
 add_filter('template_include', 'ppf_order_received_template', 99);
 
@@ -39,12 +37,19 @@ function ppf_order_received_template($template)
             return $custom_template;
         }
     }
-
     // Если мы не находимся на странице "order-received" или не найден пользовательский шаблон, возвращаем текущий шаблон
     return $template;
 }
 
+function custom_woocommerce_breadcrumbs($defaults)
+{
+    $defaults['delimiter'] = ' > '; // Вказуємо свій роздільник
+    $defaults['wrap_before'] = '<nav class="woocommerce-breadcrumb text_color_gray plain_text_small">';
+    $defaults['wrap_after'] = '</nav>';
+    return $defaults;
+}
 
+add_filter('woocommerce_breadcrumb_defaults', 'custom_woocommerce_breadcrumbs');
 
 // add_filter('category_link', function($a){
 // 	return str_replace( '/product-category', '', $a );
--- dev/wp-theme/template-parts/ppf-product-category-card.php
+++ dev/wp-theme/template-parts/ppf-product-category-card.php
@@ -1,4 +1,4 @@
-<article class="product_card">
+<a class="product_card" href="<?php echo esc_url($category_link); ?>" >
     <div class="product_card_image_container">
         <img src="<?php echo esc_url($image_url); ?>"
             alt="<?php echo esc_attr($product_category->name); ?>" class="bg_img product_card_image"
@@ -9,9 +9,9 @@
         <p class="plain_text_small text_truncated">
             <?php echo wp_strip_all_tags($product_category->description); ?>
         </p>
-        <a title="Перейти на сторінку категории <?php echo esc_attr($product_category->name); ?>"
+        <span title="Перейти на сторінку категории <?php echo esc_attr($product_category->name); ?>"
             href="<?php echo esc_url($category_link); ?>" class="sub_link">
             <span>ДОКЛАДНІШЕ</span>
-        </a>
+        </span>
     </div>
-</article>
+</a>
