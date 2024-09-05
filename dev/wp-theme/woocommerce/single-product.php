e8900cd585c95e0269a718f1eb5cb6a8ff7d07ac
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2024-09-06 00:39:57 +0300

    single-product

--- dev/wp-theme/woocommerce/single-product.php
+++ dev/wp-theme/woocommerce/single-product.php
@@ -34,29 +34,48 @@ global $product;
 <body>
     <?php get_header(); ?>
     <main>
+
+        <section class="section">
+            <div class="container">
+                <div class="single_product_card_row">
+                    <div class="single_product_card_image_cnt bg_theme">FPD</div>
+                    <div class="single_product_card_description_cnt bg_theme">
+                        <div class="row gap width_100">
+                            <h1 class="header_4 flex_1 bg_theme">Name</h1>
+                            <div class="row_end gap flex_1 bg_theme">
+                                <span>Amount</span>
+                                <span>Add to cart</span>
+                            </div>
+                        </div>
+                        <p>Description</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
         <section class="section">
             <div class="container col big_gap">
-
-                <div class="row">
-                    <? if (function_exists('woocommerce_breadcrumb')) {
-                        woocommerce_breadcrumb();
-                    } ?>
-                </div>
-                <div id="product-<?php the_ID(); ?>">
-                    <div class="col">
-                        <?php if (!do_shortcode('[fpd]')) {
-                            ?>
-                            <picture class="col Width_100">
-                                <?php $image_url = wp_get_attachment_url($product->get_image_id()); ?>
-                                <img src="<?php echo $image_url ?>" class="product_card__image bg_img width_100"
-                                    width="380" height="180" alt="<?php echo $relate_product_name; ?>">
-                            </picture>
-                            <?php
-                        } else {
-                            echo do_shortcode('[fpd]');
+                <div class="col small_gap">
+                    <div class="row">
+                        <? if (function_exists('woocommerce_breadcrumb')) {
+                            woocommerce_breadcrumb();
                         } ?>
                     </div>
-
+                    <div id="product-<?php the_ID(); ?>">
+                        <div class="col">
+                            <?php if (!do_shortcode('[fpd]')) {
+                                ?>
+                                <picture class="col width_100">
+                                    <?php $image_url = wp_get_attachment_url($product->get_image_id()); ?>
+                                    <img src="<?php echo $image_url ?>" class="product_card__image bg_img width_100"
+                                        width="380" height="180" alt="<?php echo $relate_product_name; ?>">
+                                </picture>
+                                <?php
+                            } else {
+                                echo do_shortcode('[fpd]');
+                            } ?>
+                        </div>
+                    </div>
                     <div class="col gap">
                         <p class="text_24">
                             <?php echo $product->get_name(); ?>
@@ -105,86 +124,91 @@ global $product;
                     </div>
                 </div>
             </div>
-            <div class="col width_100">
-                <p class="text_24">Асоційовані товари:</p>
-                <div class="row big_gap width_100">
-                    <?php
-                    $upsells = $product->get_upsell_ids();
-                    if (!empty($upsells)) {
-                        foreach ($upsells as $upsell_id) {
-                            $upsell = wc_get_product($upsell_id);
-                            $upsell_product_name = $upsell->get_name();
-                            $upsell_image_url = wp_get_attachment_url($upsell->get_image_id());
-                            ?>
-                            <div class="product_card cards_3">
-                                <picture>
-                                    <img src="<?php echo $upsell_image_url ?>" class="product_card__image bg_img width_100"
-                                        width="380" height="180" alt="<?php echo $upsell_product_name; ?>">
-                                </picture>
-                                <h3 class="text_24 width_100">
-                                    <?php echo $upsell_product_name; ?>
-                                </h3>
-                                <p class="text_14 width_100">
-                                    <?php echo $upsell->get_short_description(); ?>
-                                </p>
-                                <div class="row_sp_btw">
-                                    <span class="price">
-                                        <?php echo $upsell->get_price_html(); ?>
-                                    </span>
-                                    <a title="Натисніть щоб перейти до сторінки замовлення <?php echo $upsell_product_name; ?>"
-                                        href="<?php echo get_permalink($upsell_id); ?>" class="sub_link">
-                                        <span>ЗАМОВИТИ</span>
-                                    </a>
+        </section>
+        <section class="section">
+            <div class="container col big_gap">
+                <div class="col width_100">
+                    <p class="text_24">Асоційовані товари:</p>
+                    <div class="row big_gap width_100">
+                        <?php
+                        $upsells = $product->get_upsell_ids();
+                        if (!empty($upsells)) {
+                            foreach ($upsells as $upsell_id) {
+                                $upsell = wc_get_product($upsell_id);
+                                $upsell_product_name = $upsell->get_name();
+                                $upsell_image_url = wp_get_attachment_url($upsell->get_image_id());
+                                ?>
+                                <div class="product_card cards_3">
+                                    <picture>
+                                        <img src="<?php echo $upsell_image_url ?>" class="product_card__image bg_img width_100"
+                                            width="380" height="180" alt="<?php echo $upsell_product_name; ?>">
+                                    </picture>
+                                    <h3 class="text_24 width_100">
+                                        <?php echo $upsell_product_name; ?>
+                                    </h3>
+                                    <p class="text_14 width_100">
+                                        <?php echo $upsell->get_short_description(); ?>
+                                    </p>
+                                    <div class="row_sp_btw">
+                                        <span class="price">
+                                            <?php echo $upsell->get_price_html(); ?>
+                                        </span>
+                                        <a title="Натисніть щоб перейти до сторінки замовлення <?php echo $upsell_product_name; ?>"
+                                            href="<?php echo get_permalink($upsell_id); ?>" class="sub_link">
+                                            <span>ЗАМОВИТИ</span>
+                                        </a>
+                                    </div>
                                 </div>
-                            </div>
-                            <?php
-                        }
-                    } ?>
+                                <?php
+                            }
+                        } ?>
+                    </div>
                 </div>
+
             </div>
+        </section>
+        <section class="section">
+            <div class="container col big_gap">
+                <div class="col width_100">
+                    <p class="text_24">Рекомендовані товари:</p>
+                    <div class="row big_gap width_100">
+                        <?php
+                        $related = $product->get_related();
+                        if (!empty($related)) {
+                            foreach ($related as $related_id) {
+                                $related_product = wc_get_product($related_id);
+                                $relate_product_name = $related_product->get_name();
 
-            <div class="col width_100">
-                <p class="text_24">Рекомендовані товари:</p>
-                <div class="row big_gap width_100">
-                    <?php
-                    $related = $product->get_related();
-                    if (!empty($related)) {
-                        foreach ($related as $related_id) {
-                            $related_product = wc_get_product($related_id);
-                            $relate_product_name = $related_product->get_name();
+                                $image_url = wp_get_attachment_url($related_product->get_image_id());
 
-                            $image_url = wp_get_attachment_url($related_product->get_image_id());
-
-                            ?>
-                            <div class="product_card cards_3">
-                                <picture>
-                                    <img src="<?php echo $image_url ?>" class="product_card__image bg_img width_100"
-                                        width="380" height="180" alt="<?php echo $relate_product_name; ?>">
-                                </picture>
-                                <h3 class="text_24 width_100">
-                                    <?php echo $relate_product_name; ?>
-                                </h3>
-                                <p class="text_14 width_100">
-                                    <?php echo $related_product->get_short_description(); ?>
-                                </p>
-                                <div class="row_sp_btw">
-                                    <span class="price">
-                                        <?php echo $related_product->get_price_html(); ?>
-                                    </span>
-                                    <a title="Натисніть щоб перейти до сторінки замовлення <?php echo $relate_product_name; ?>"
-                                        href="<?php echo get_permalink($related_id); ?>" class="sub_link">
-                                        <span>ЗАМОВИТИ</span>
-                                    </a>
+                                ?>
+                                <div class="product_card cards_3">
+                                    <picture>
+                                        <img src="<?php echo $image_url ?>" class="product_card__image bg_img width_100"
+                                            width="380" height="180" alt="<?php echo $relate_product_name; ?>">
+                                    </picture>
+                                    <h3 class="text_24 width_100">
+                                        <?php echo $relate_product_name; ?>
+                                    </h3>
+                                    <p class="text_14 width_100">
+                                        <?php echo $related_product->get_short_description(); ?>
+                                    </p>
+                                    <div class="row_sp_btw">
+                                        <span class="price">
+                                            <?php echo $related_product->get_price_html(); ?>
+                                        </span>
+                                        <a title="Натисніть щоб перейти до сторінки замовлення <?php echo $relate_product_name; ?>"
+                                            href="<?php echo get_permalink($related_id); ?>" class="sub_link">
+                                            <span>ЗАМОВИТИ</span>
+                                        </a>
+                                    </div>
                                 </div>
-                            </div>
-                            <?php
+                                <?php
+                            }
                         }
-                    }
-                    ?>
+                        ?>
+                    </div>
                 </div>
-            </div>
-
-
             </div>
         </section>
     </main>
